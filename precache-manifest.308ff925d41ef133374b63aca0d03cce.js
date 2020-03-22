077a9c2eb35753e3dd19b8e11f0738b577cf3268
leandroromano <leandrolromano@gmail.com>
2020-03-22 12:34:29 -0300

    Updates

---
+++ precache-manifest.308ff925d41ef133374b63aca0d03cce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce286759ce163a633f128f39fbb2f2e1",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "cd906499f4e0cca061ee",
+    "url": "/my-app/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "cd906499f4e0cca061ee",
+    "url": "/my-app/static/js/2.b04e8215.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-app/static/js/2.b04e8215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06f97f56d70452f8fc90",
+    "url": "/my-app/static/js/main.d8338385.chunk.js"
+  },
+  {
+    "revision": "1b58fcb5ce8d26145ee7",
+    "url": "/my-app/static/js/runtime-main.8b909c6e.js"
+  }
+]);
